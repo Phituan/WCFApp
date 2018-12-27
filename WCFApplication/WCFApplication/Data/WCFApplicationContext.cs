b0066057bf8adf62169d508e2c7c19564a5e192e
ok <tuandpd00617@fpt.edu.vn>
2018-12-27 12:12:24 +0700

    nop-bai

---
+++ WCFApplication/WCFApplication/Data/WCFApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WCFApplication.Models
+{
+    public class WCFApplicationContext : DbContext
+    {
+        public WCFApplicationContext (DbContextOptions<WCFApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WCFApplication.Models.Employee> Employee { get; set; }
+    }
+}
